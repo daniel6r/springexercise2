02322aeb809b8dcad718154b09448d2b6b8337fd
daniel <itsdaniel@protonmail.com>
2023-06-10 20:54:21 +0200

    initial commit

---
+++ src/test/java/com/example/springexercise2/Springexercise2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springexercise2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springexercise2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
