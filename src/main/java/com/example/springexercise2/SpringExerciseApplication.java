309d2773872bbec42703a97f5ac48ba215f7cfbd
daniel <itsdaniel@protonmail.com>
2023-06-10 21:48:11 +0200

    added default values to application.properties and value injection through constructor of registrationService

--- src/main/java/com/example/springexercise2/SpringExerciseApplication.java
+++ src/main/java/com/example/springexercise2/SpringExerciseApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringExerciseApplication {
